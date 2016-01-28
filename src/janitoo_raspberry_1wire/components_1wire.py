86cb7486e155991167db656bce9e093ec7dc3ab8
bibi21000 <bibi21000@gmail.com>
2016-01-28 18:39:07 +0100

    Auto-commit

---
+++ src/janitoo_raspberry_1wire/components_1wire.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+"""The Raspberry camera worker
+
+Installation :
+
+.. code-block:: bash
+
+    sudo apt-get install python-pycamera
+
+"""
+
+__license__ = """
+    This file is part of Janitoo.
+
+    Janitoo is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Janitoo is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Janitoo. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+__author__ = 'Sébastien GALLET aka bibi21000'
+__email__ = 'bibi21000@gmail.com'
+__copyright__ = "Copyright © 2013-2014-2015 Sébastien GALLET aka bibi21000"
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+logger = logging.getLogger(__name__)
+import os, sys
+import threading
+import time
+import datetime
+import socket
+import re
+
+from janitoo.thread import JNTBusThread
+from janitoo.bus import JNTBus
+from janitoo.component import JNTComponent
+from janitoo.thread import BaseThread
+from janitoo.options import get_option_autostart
+
+##############################################################
+#Check that we are in sync with the official command classes
+#Must be implemented for non-regression
+from janitoo.classes import COMMAND_DESC
+
+COMMAND_CAMERA_PREVIEW = 0x2200
+COMMAND_CAMERA_PHOTO = 0x2201
+COMMAND_CAMERA_VIDEO = 0x2202
+COMMAND_CAMERA_STREAM = 0x2203
+
+assert(COMMAND_DESC[COMMAND_CAMERA_PREVIEW] == 'COMMAND_CAMERA_PREVIEW')
+assert(COMMAND_DESC[COMMAND_CAMERA_PHOTO] == 'COMMAND_CAMERA_PHOTO')
+assert(COMMAND_DESC[COMMAND_CAMERA_VIDEO] == 'COMMAND_CAMERA_VIDEO')
+assert(COMMAND_DESC[COMMAND_CAMERA_STREAM] == 'COMMAND_CAMERA_STREAM')
+##############################################################
+
+def make_ds18B20(**kwargs):
+    return DS18B20(**kwargs)
+
+class DS18B20(JNTComponent):
+    """ A generic component for gpio """
+
+    def __init__(self, bus=None, addr=None, **kwargs):
+        """
+        """
+        oid = kwargs.pop('oid', 'rpi1wire.ds18B20')
+        name = kwargs.pop('name', "DS18B20 sensor")
+        product_name = kwargs.pop('product_name', "DS18B20")
+        product_type = kwargs.pop('product_type', "Temperature")
+        product_manufacturer = kwargs.pop('product_manufacturer', "Janitoo")
+        JNTComponent.__init__(self, oid=oid, bus=bus, addr=addr, name=name,
+                product_name=product_name, product_type=product_type, product_manufacturer="Janitoo", **kwargs)
+        logger.debug("[%s] - __init__ node uuid:%s", self.__class__.__name__, self.uuid)
+
+        uuid="temperature"
+        self.values[uuid] = self.value_factory['sensor_temperature'](options=self.options, uuid=uuid,
+            node_uuid=self.uuid,
+            help='The temperature',
+            label='Temp',
+            get_data_cb=self.temperature,
+        )
+        poll_value = self.values[uuid].create_poll_value()
+        self.values[poll_value.uuid] = poll_value
+
+        uuid="hexadd"
+        self.values[uuid] = self.value_factory['config_string'](options=self.options, uuid=uuid,
+            node_uuid=self.uuid,
+            help='The hexadecimal address of the DS18B20',
+            label='Addr',
+            default="28-000005e2fdc3",
+        )
+
+    def temperature(self, node_uuid, index):
+        data = None
+        ret = None
+        try:
+            f=open(os.path.join(self.bus.values["sensors_dir"].data, temp_sensor, self.values["hexadd"].instances[index]['data'], 'w1_slave'), 'r')
+            line = f.readline()
+            if re.match(r"([0-9a-f]{2} ){9}: crc=[0-9a-f]{2} YES", line):
+                line = f.readline()
+                m = re.match(r"([0-9a-f]{2} ){9}t=([+-]?[0-9]+)", line)
+                if m:
+                    data = str(float(m.group(2)) / 1000.0)
+            f.close()
+            ret = float(data)
+        except (IOError), e:
+            logger.exception('Exception when reading sensor')
+        return ret
